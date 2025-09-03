eed34d544c60f700f984ed3d77efaf72a85bbc78
bibash21-creator <bibashworkmail21@gmail.com>
2025-09-03 22:57:23 +0545

    Added warning

--- app.py
+++ app.py
@@ -109,9 +109,7 @@ if st.button("Check Resut"):
      #   st.toast("🧨 Error: Voltage too low!", icon="❌")
        st.snow()
        st.error("If this  is  your Roll Number, you are failed brother. Better luck next  time!")
-       st.warning("Better check your Roll Number Once if you mistyped.")
-       with st.spinner("Student Not Found. Sorry!"):
-          st.warning("Sorry No Result Found")
+       st.warning("Better check your typed details if mistyped.")
     
 
 
